01b91d0fb3f119529423895768f32dce1d6388d6
Игорь <markevichihar@gmail.com>
2022-07-25 21:55:21 +0300

    next3

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -3,27 +3,28 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
+		try (ServerSocket server = new ServerSocket(8080)) {
+			System.out.println("Server started!");
 
-		ServerSocket server = new ServerSocket(8080);
-
-		System.out.println("Server started!");
-
-		Socket socket = server.accept();
-
-		System.out.println("Client connected!");
-
-		BufferedWriter writer =
-				new BufferedWriter(
-						new OutputStreamWriter(
-								socket.getOutputStream()));
-		writer.write("HELLO FROM SERVER");
-		writer.newLine();
-		writer.flush();
-
-		writer.close();
-		socket.close();
-		server.close();
+			try(Socket socket = server.accept();
+					BufferedWriter writer =
+						new BufferedWriter(
+								new OutputStreamWriter(
+										socket.getOutputStream()));
+				BufferedReader reader =
+						new BufferedReader(
+								new InputStreamReader(
+										socket.getInputStream()));
+			){
+				String request = reader.readLine();
+			writer.write("HELLO FROM SERVER: " + request.length());
+			writer.newLine();
+			writer.flush();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
